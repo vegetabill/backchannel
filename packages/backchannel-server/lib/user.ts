a1f64a512f21ee51f4fe964b397c804d71513f0e
Bill D <355438+vegetabill@users.noreply.github.com>
2020-05-16 17:04:51 -0700

    replace random names with madlib-style adj+gender neutral baby names json

--- packages/backchannel-server/lib/user.ts
+++ packages/backchannel-server/lib/user.ts
@@ -1,12 +1,29 @@
 import * as shortId from "shortid";
-import * as Chance from "chance";
+import { shuffle } from "lodash";
 import { User } from "backchannel-common";
 
-const chance = new Chance();
+const seeds = require("./usernames.json");
+
+function* nameGenerator(): Generator<string, string, string> {
+  const names = [];
+  const adjectives = [];
+
+  while (true) {
+    if (names.length === 0) {
+      names.push(...shuffle(seeds.names));
+    }
+    if (adjectives.length === 0) {
+      adjectives.push(...shuffle(seeds.adjectives));
+    }
+    yield [adjectives.pop(), names.pop()].join(" ");
+  }
+}
+
+const generator = nameGenerator();
 
 export function generateUser(): User {
   return {
     id: shortId.generate(),
-    name: chance.first(),
+    name: generator.next().value,
   };
 }
